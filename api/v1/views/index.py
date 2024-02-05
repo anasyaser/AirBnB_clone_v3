fab99b77da475b3f3830fa014b1926324a1e4cd1
anasyaser <anoos649@gmail.com>
2024-02-05 20:22:21 +0200

    GET stats count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,16 @@ from api.v1.views import app_views
 def status():
     """Return the status"""
     return jsonify({"status": "OK"}), 200
+
+@app_views.route('stats')
+def get_stats():
+    """Retrieves the number of each objects"""
+    stats = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    return jsonify({name: storage.count(cls) for cls, name in stats.items()})
